362395d3cccd818b09e8e4d7604e8b17a2de8728
Monica Mayumi Gushiken <monica.gushiken@gmail.com>
2021-01-28 21:05:22 -0300

    Aula 8 - utilizando given when then

--- curso-restassured-udemy/src/main/java/br/com/mmg/rest/OlaMundoTest.java
+++ curso-restassured-udemy/src/main/java/br/com/mmg/rest/OlaMundoTest.java
@@ -1,5 +1,9 @@
 package br.com.mmg.rest;
 
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.request;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -15,7 +19,7 @@ public class OlaMundoTest {
 		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
 		Assert.assertTrue(response.statusCode() == 200);
 		Assert.assertTrue("O status deveria ser 200", response.statusCode() == 200);
-		Assert.assertEquals(201, response.statusCode());
+		Assert.assertEquals(200, response.statusCode());
 
 		/*
 		 * Quando há uma exceção antes do teste, dá Errors:1 e Failures:0 Quando o teste
@@ -23,6 +27,24 @@ public class OlaMundoTest {
 		 */
 
 		ValidatableResponse validacao = response.then();
-		validacao.statusCode(201);
+		validacao.statusCode(200);
+	}
+	
+	@Test
+	public void devoConhecerOutrasFormasRestAssured() {
+		Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+		
+		// Requisição simplificada
+		get("http://restapi.wcaquino.me/ola").then().statusCode(200);
+		
+		// Modo fluente - given when then
+		given() // Pré-condições
+		.when() // Ação
+			.get("http://restapi.wcaquino.me/ola")
+		.then() // Assertivas
+			.statusCode(200);
+		
 	}
 }
