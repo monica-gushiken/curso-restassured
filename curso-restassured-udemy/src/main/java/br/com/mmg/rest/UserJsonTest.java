3148e8c694fedfec88fa5990e39cb043384b68c9
Monica Mayumi Gushiken <monica.gushiken@gmail.com>
2021-03-21 11:48:08 -0300

    Aula 14 - Validando retorno 404 e mensagem de erro no response

--- curso-restassured-udemy/src/main/java/br/com/mmg/rest/UserJsonTest.java
+++ curso-restassured-udemy/src/main/java/br/com/mmg/rest/UserJsonTest.java
@@ -71,4 +71,15 @@ public class UserJsonTest {
 			
 			;
 	}
+	
+	@Test
+	public void deveRetornarErroUsuarioInexistente() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users/4")
+		.then()
+			.statusCode(404)
+			.body("error", is("Usuário inexistente"))
+			;
+	}
 }
